61dfb873fcc9c801f3f3df7789bf5b7e3f2a177d
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-07-11 23:55:28 -0300

    Domain Model

--- src/main/java/com/localhost/dslist/services/GameService.java
+++ src/main/java/com/localhost/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.localhost.dslist.dto.GameDTO;
 import com.localhost.dslist.dto.GameMinDTO;
 import com.localhost.dslist.entities.Game;
 import com.localhost.dslist.repositories.GameRepository;
@@ -14,10 +16,10 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll () {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dtoList = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dtoList;
 	}
-
 }
