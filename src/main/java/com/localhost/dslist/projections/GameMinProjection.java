222c975688ff63c42e34e45801fc81a64c7ebfc9
Alexandre Ferreira <alexandrejfneto@gmail.com>
2023-07-12 22:18:32 -0300

    Correção campo Year no SQL

--- src/main/java/com/localhost/dslist/projections/GameMinProjection.java
+++ src/main/java/com/localhost/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.localhost.dslist.projections;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
